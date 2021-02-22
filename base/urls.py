dbd7903717fc96919f685643c64777d31a499653
Bastin99 <71763120+Bastin99@users.noreply.github.com>
2021-02-22 12:06:01 +0530

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('quizes.urls',namespace='quizes')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
